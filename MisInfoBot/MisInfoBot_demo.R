1c5dfc98384a50d80dfe94562432cca574497e5f
Declan Casey <dcasey@decaseyair2015.local>
2020-07-27 11:24:41 -0400

    edit MisInfoBot.R

--- MisInfoBot/MisInfoBot_demo.R
+++ MisInfoBot/MisInfoBot_demo.R
@@ -16,9 +16,8 @@ library('rcorpora')
 library('radlibs')
 library('qdapDictionaries')
 
+# Declare the 'iterations' and 'demo_list' variables, then copy and paste the rest of the code in the console and run it.
 iterations <- 15
-
-# Creating a demonstration list of sentences to test the for loop on
 demo_list <- list("Two roads diverged in a yellow wood",
                   "And sorry I could not travel both",
                   "And be one traveler, long I stood",
@@ -42,33 +41,34 @@ demo_list <- list("Two roads diverged in a yellow wood",
                   "Two roads diverged in a wood, and I—",
                   "I took the one less traveled by,",
                   "And that has made all the difference.")
+
+
+    # Format the demo_list for parsing and write the original to a .txt file
+write(unlist(demo_list), "old_article_demo.txt", sep = "\n")
 demo_list <- str_split(demo_list, " ")
 demo_list <- setNames(demo_list, paste0("p", seq_along(demo_list)))
-
-# Pull a nested list of adjectives from rcopora and convert the adjectives to their own list.
+    # Pull a nested list of adjectives from rcopora and convert the adjectives to their own list.
 adjs <- corpora('words/adjs')
 adjs <- setNames(as.list(adjs$adjs), paste0("adj", seq_along(adjs$adjs)))
-# Pull a nested list of adverbs from rcopora and comvert the adverbs to their own list.
+    # Pull a nested list of adverbs from rcopora and comvert the adverbs to their own list.
 adverbs <- corpora('words/adverbs')
 adverbs <- setNames(as.list(adverbs$adverbs), paste0("adv", seq_along(adverbs$adverbs)))
-# Pull a nested list of nouns from rcopora and convert the nouns to their own list.
+    # Pull a nested list of nouns from rcopora and convert the nouns to their own list.
 nouns <- corpora('words/nouns')
 nouns <- setNames(as.list(nouns$nouns), paste0("n", seq_along(nouns$nouns)))
-# Pull a dataframe of proper nouns and save them as a list
+    # Pull a dataframe of proper nouns and save them as a list
 data("proper_nouns")
 proper_nouns <- force(proper_nouns)
 proper_nouns <- setNames(as.list(proper_nouns$word), paste0("pr", seq_along(proper_nouns$word)))
-# Pull a dataframe of past and present verbs and save them as lists
+    # Pull a dataframe of past and present verbs and save them as lists
 verbs <- corpora('words/verbs')
 verbs <- verbs$verbs
 verbs_present <- setNames(as.list(verbs$present), paste0("v", seq_along(verbs$present)))
 verbs_past <- setNames(as.list(verbs$past), paste0("v", seq_along(verbs$past)))
-# Pull a list of prepositions from the 'qdapDictionaries' library
+    # Pull a list of prepositions from the 'qdapDictionaries' library
 data(preposition)
 prepositions <- setNames(as.list(preposition), paste0("prep", seq_along(preposition)))
-
-# The for loop to use on the demonstration list
-# Remove the comments and copy and paste if you would like to see the changes on a smaller sample
+    # The for loop to use on the demonstration list
 for (i in 1:iterations){ # for a specific change in the total amount of changes
   LIST_OF_UNACCEPTABLE_POS <- list("AUX", "CONJ", "DET", "INTJ", "NUM", "PART", "PROPN", "PUNCT", "SCONJ", "SYM",
                                    "X",  "CCONJ", "SCONJ")
@@ -114,11 +114,7 @@ for (i in 1:iterations){ # for a specific change in the total amount of changes
     }
   }
 }
-
-# Turning the demonstration list into a txt file
-
-# Remove the whitespace in between the words
+    # Remove the whitespace in between the words
 paragraph_list <- lapply(demo_list, paste, collapse = " ")
-
-# Write the new paragraphs to a txt file
+    # Write the new paragraphs to a txt file
 write(unlist(paragraph_list), "new_article_demo.txt", sep = "\n")
