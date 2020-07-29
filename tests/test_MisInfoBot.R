83be5330315e2ff404336da2fcab11fe43493f8b
Declan Casey <declancasey@Declans-MacBook-Air.local>
2020-07-29 11:54:26 -0400

    add test case to check that iterations is a number

--- tests/test_MisInfoBot.R
+++ tests/test_MisInfoBot.R
@@ -3,20 +3,20 @@
 source("/Users/declancasey/Desktop/Summer2020/misinformation/MisInfoBot/MisInfoBot.R", chdir = TRUE)
 library('testthat')
 
-
-
-# pretty sure this works
 test_that("paragraphs_list contains a nested list", {
   paragraphs_list <- readNYTLinkToParagraphs(url)
   expect_output(str(paragraphs_list), "List of")
 })
 
-# pretty sure this works
 test_that("psw_list contains a nested list", {
   psw_list <- readNYTLinkToPSW(url)
   expect_output(str(psw_list), "List of")
 })
 
-test_that("url contains an New York Times url",{
+test_that("url contains an New York Times url", {
   expect_match(url, "https://www.nytimes.com/")
+})
+
+test_that("iterations is a number", {
+  expect_output(str(iterations), "num")
 })
