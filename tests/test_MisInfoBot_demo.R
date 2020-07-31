591447b0b74138a91c158c5bfeea4b8e307fc5d5
Declan Casey <declancasey@Declans-MacBook-Air.local>
2020-07-31 10:39:59 -0400

    add test cases for MisInfoBot_demo and moved R library tutorials to resources

---
+++ tests/test_MisInfoBot_demo.R
@@ -0,0 +1,12 @@
+# install.packages('testthat')
+
+source("/Users/declancasey/Desktop/Summer2020/misinformation/MisInfoBot/MisInfoBot_demo.R", chdir = TRUE)
+library('testthat')
+
+test_that("iterations is a number", {
+  expect_output(str(iterations), "num")
+})
+
+test_that("demo_list contains a list", {
+  expect_output(str(demo_list), "List")
+})
